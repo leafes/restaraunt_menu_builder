31f55c437599bc8cbf4a50eb5eadc01b2c9e56df
leafes <xmm.geek@gmail.com>
2023-01-25 22:42:09 +0700

    get rid of test file

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const getMenu = async () => {
-  const f = await fetch("http://localhost:8000/api/v1/menus/");
-  const result = await f.json();
-  console.log(result);
-  return result;
-}
-
-getMenu();
-
