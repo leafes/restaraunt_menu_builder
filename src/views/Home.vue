24fe79a9ada36a24b0428e5db7faf031bcf33b6e
leafes <xmm.geek@gmail.com>
2023-01-27 20:31:27 +0700

    minor style changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center mt-28">
-    <h1 class="text-2xl">That's a start page</h1> <br />
+    <h1 class="text-2xl">This is a start page</h1> <br />
     Welcome to the menu editor! <br />
     <router-link to="/create">Create</router-link> |
     <router-link to="/menu">View menu</router-link> |
