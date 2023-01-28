5b3df84e2a0a9b3e44a293d060f2685576ddd418
leafes <xmm.geek@gmail.com>
2023-01-29 03:06:57 +0700

    simple token recieving

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,19 @@
 <template>
   <input
     type="text"
+    v-model="login"
     placeholder="Login"
     class="border-b p-2 mb-4 focus:border-slate-800 outline-none"
   />
   <br />
   <input
     @keydown.enter="signUp"
+    v-model="password"
     type="password"
     placeholder="Password"
     class="border-b p-2 mb-4 focus:border-slate-800 outline-none"
   />
-  {{ access_token }}
+  {{ test }}
 </template>
 <script>
 export default {
@@ -19,23 +21,25 @@ export default {
     return {
       login: '',
       password: '',
-      access_token: ''
+      access_token: '',
+      test: {}
     }
   },
   methods: {
     async signUp() {
-      const user = { email: `${this.login}@bibkin.com`, username: this.login, password: this.password };
-      const f = await fetch('http://localhost:8000/api/v1/auth/sign-up', {
-        method: "POST",
+      const user = { email: `${this.login}@gmail.com`, username: this.login, password: this.password };
+      this.test = user;
+        const newBody = JSON.stringify(user);
+        const response = await fetch(`http://localhost:8000/api/v1/auth/sign-up`, {
+          method: "POST",
           headers: {
             Accept: "application/json",
             "Content-type": "application/json",
           },
-          body: user,
-        });
-      const result = await f.json();
-      this.access_token = result.access_token;
-    }
+          body: newBody,
+        })
+        this.test = await response.json();
+      }
   }
 }
 </script>
