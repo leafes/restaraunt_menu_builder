1bc239414b77f5454ef60a694fa01fd237e0e915
leafes <xmm.geek@gmail.com>
2023-01-29 21:02:34 +0700

    saveNewMenu refactor

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -8,43 +8,43 @@
       type="text"
       class="border-b p-2 mb-4 focus:border-slate-800 outline-none w-full"
       placeholder="Menu name"
-      v-model="menuName"
+      v-model="menu.title"
     />
   </div>
   <input
     type="text"
     class="border-b p-2 mb-4 focus:border-slate-800 outline-none"
     placeholder="Submenu name"
-    v-model="newSubmenuName"
+    v-model="newSubmenuTitle"
     @keydown.enter="addSubMenu()"
   />
   <div
     class="shadow-md p-2 mb-2"
-    v-for="submenu in subMenus"
-    :key="submenu.name"
+    v-for="submenu in menu.submenus"
+    :key="submenu.title"
   >
-    {{ submenu.name }}
+    {{ submenu.title }}
     <div
       class="p-2 flex mb-2 bg-slate-50"
-      v-for="meal in submenu.meals"
-      :key="meal.name"
+      v-for="dish in submenu.dishes"
+      :key="dish.title"
     >
-      {{ meal.name }} | ${{ meal.price }}
+      {{ dish.title }} | ${{ dish.price }}
     </div>
     <div class="p-2 flex opacity-50 hover:opacity-100 group-focus:opacity-100">
       <input
         type="text"
         placeholder="Meal name"
         class="border-b focus:border-slate-800 outline-none"
-        v-model="newMealNames[submenu.name]"
+        v-model="newMeals[submenu.title].title"
       />
       <input
         type="number"
         placeholder="Price"
         class="ml-2 border-b focus:border-slate-800 outline-none w-16"
-        v-model="newMealPrices[submenu.name]"
+        v-model="newMeals[submenu.title].price"
       />
-      <button @click="addMeal(submenu.name)">
+      <button @click="addMeal(submenu.title)">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -70,83 +70,42 @@
       Save menu
     </button>
   </div>
+  <!-- Menu: {{ menu }}
   Meals to add:
-  {{ newMealNames }}
-  {{ newMealPrices }}
-  {{ testData }}
+  {{ newMeals }}
+  {{ testData }} -->
 </template>
 <script>
+import { createMenu } from '../services/api-fetch';
 export default {
   data() {
     return {
-      menuName: "",
-      menuId: 0,
-      newSubmenuName: "",
+      menu: { title: "", description: "", submenus: [] },
+      newSubmenuTitle: "",
       testData: {},
-      newMealNames: {},
-      newMealPrices: {},
+      newMeals: {},
       subMenus: [],
     };
   },
   methods: {
-    addMeal(submenuName) {
-      if (!this.newMealNames[submenuName] || !this.newMealPrices[submenuName])
-        return;
-      const submenuToChange = this.subMenus.find(
-        (submenu) => submenu.name === submenuName
+    addMeal(submenuTitle) {
+      const submenuToUpdate = this.menu.submenus.find(
+        (submenu) => submenu.title === submenuTitle
       );
-      submenuToChange.meals.push({
-        name: this.newMealNames[submenuName],
-        price: Number(this.newMealPrices[submenuName]),
-      });
-      this.newMealNames[submenuName] = "";
-      this.newMealPrices[submenuName] = "";
+      const newMeal = this.newMeals[submenuTitle];
+      submenuToUpdate.dishes.push(newMeal); 
     },
     addSubMenu() {
       const newSubMenu = {
-        name: this.newSubmenuName,
-        id: this.subMenus.length,
-        meals: [],
+        title: this.newSubmenuTitle,
+        dishes: [],
       };
-      this.subMenus.push(newSubMenu);
-      this.newSubmenuName = "";
+      this.menu.submenus.push(newSubMenu);
+      this.newMeals[this.newSubmenuTitle] = { title: '', description: '', price: '' };
+      this.newSubmenuTitle = "";
     },
     async saveNewMenu() {
-      const postToDB = async (obj, path) => {
-        const newBody = JSON.stringify(obj);
-        const response = await fetch(`http://localhost:8000/api/v1/${path}`, {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-type": "application/json",
-          },
-          body: newBody,
-        });
-
-        return await response.json();
-      };
-
-      const createMenuData = await postToDB(
-        { title: this.menuName, description: "bibaboba" },
-        "menus/"
-      );
-      const pathForThisMenu = `menus/${createMenuData.id}/submenus/`;
-
-      this.subMenus.forEach(async (submenu) => {
-        const submenuData = await postToDB(
-          { title: submenu.name, description: "add meals iter 2" },
-          pathForThisMenu
-        );
-        const pathForThisSubMenu = `${pathForThisMenu}${submenuData.id}/dishes/`;
-
-        submenu.meals.forEach(
-          async (meal) =>
-            await postToDB(
-              { title: meal.name, price: meal.price, description: "bum" },
-              pathForThisSubMenu
-            )
-        );
-      });
+      this.testData = await createMenu(this.menu);
     },
   },
   // created() {
