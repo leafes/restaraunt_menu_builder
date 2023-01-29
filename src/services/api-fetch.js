e0d0fb19860f99e4444a08c770433b5449681e67
leafes <xmm.geek@gmail.com>
2023-01-29 20:24:43 +0700

    add createMenu func

--- src/services/api-fetch.js
+++ src/services/api-fetch.js
@@ -9,13 +9,14 @@ const get = async (path) => {
   }
 };
 
-const post = async (path, requestBody, token = "") => {
+const post = async (path, requestBody, token = null) => {
   try {
     const response = await fetch(`${HOST}${path}`, {
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-type": "application/json",
+        Authorization: token,
       },
       body: JSON.stringify(requestBody),
     });
@@ -28,4 +29,28 @@ const post = async (path, requestBody, token = "") => {
 export const signUp = async (user) => await post("/api/v1/auth/sign-up", user);
 export const signIn = async (user) => await post("/api/v1/auth/sign-in", user);
 export const getMenu = async (id) => await get(`/api/v1/menus/${id}`);
-export const createMenu = async (menu) => await post(`/api/v1/menus/`, menu);
+
+export const createMenu = async (menu) => {
+  const createMenuData = await post("menus/", {
+    title: menu.title,
+    description: menu.description,
+  });
+  const pathForThisMenu = `menus/${createMenuData.id}/submenus/`;
+
+  menu.submenus.forEach(async (submenu) => {
+    const submenuData = await post(pathForThisMenu, {
+      title: submenu.name,
+      description: submenu.description,
+    });
+    const pathForThisSubMenu = `${pathForThisMenu}${submenuData.id}/dishes/`;
+
+    submenu.meals.forEach(
+      async (meal) =>
+        await post(pathForThisSubMenu, {
+          title: meal.name,
+          price: meal.price,
+          description: meal.description,
+        })
+    );
+  });
+};
