c1f1f47acdb4e48a6097823f41789781897b2b85
leafes <xmm.geek@gmail.com>
2023-02-10 19:42:20 +0700

    login redesign

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,11 @@
 <template>
+  <div class="m-auto mt-32 text-center w-60">
+    <h1 class="text-xl mb-4">Sign in</h1>
   <input
     type="text"
     v-model="login"
     placeholder="Login"
-    class="border-b p-2 mb-4 focus:border-slate-800 outline-none"
+    class="border-b p-2 mb-4 focus:border-slate-800 outline-none w-full"
   />
   <br />
   <input
@@ -11,9 +13,12 @@
     v-model="password"
     type="password"
     placeholder="Password"
-    class="border-b p-2 mb-4 focus:border-slate-800 outline-none"
+    class="border-b p-2 mb-4 focus:border-slate-800 outline-none w-full"
   />
+  <br>
+  <button class="bg-black mt-4 text-white p-2.5 w-full rounded-lg">Sign in</button>
   {{ test }}
+</div>
 </template>
 <script>
 import { signIn, signUp } from "../services/api-fetch";
