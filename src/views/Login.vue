2a2e11b5f47093460d900fde939d96a93e79741f
leafes <xmm.geek@gmail.com>
2023-02-10 19:45:50 +0700

    login redesign

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-auto mt-32 text-center w-60">
-    <h1 class="text-xl mb-4">Sign in</h1>
+    <h1 class="text-xl mb-4">Sign up</h1>
   <input
     type="text"
     v-model="login"
@@ -16,7 +16,8 @@
     class="border-b p-2 mb-4 focus:border-slate-800 outline-none w-full"
   />
   <br>
-  <button class="bg-black mt-4 text-white p-2.5 w-full rounded-lg">Sign in</button>
+  <button class="bg-black mt-4 text-white p-2.5 w-full rounded-lg">Sign up</button>
+  <div class="mt-4">Have an account? Sign in</div>
   {{ test }}
 </div>
 </template>
